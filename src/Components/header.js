89a98c2070133bbc397a81bc602e2f58080da1ec
Luke Haines <lukehaines2@gmail.com>
2020-02-06 02:08:45 +0000

    add vertical nav and header incrementers

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Badge, Button } from 'reactstrap';
 
-const Header = (props) => (
-  <nav className="navbar navbar-dark bg-dark">
-    <a className="navbar-brand" href="#">Hidden brand</a>
-    <button onClick={props.toggleNavbar} className="navbar-toggler" type="button" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  </nav>
-)
+
+const Header = (props) => {
+
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">Hidden brand</a>
+      <div>
+        <Button color="primary" outline className="mr-3" onClick={increment}>
+          Notifications <Badge color="secondary">{count}</Badge>
+        </Button>
+        <Button color="danger" outline className="mr-3" onClick={decrement}>
+          Take 1 away <Badge color="secondary">{count}</Badge>
+        </Button>
+        <button onClick={props.toggleNavbar} className="navbar-toggler" type="button" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+  )
+}
 
 export default Header;
--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 import cx from 'classnames';
 import '../style/sidebar.scss';
 
@@ -10,6 +11,33 @@ const Sidebar = (props) => {
       <nav className="navbar sidebar-header">
         <span onClick={props.toggleNavbar}>&times;</span>
       </nav>
+
+      <Nav vertical>
+        <NavItem>
+          <NavLink href="#">Page 1</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">View 1</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Page 2</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Page 3</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Page 4</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">LINK</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Link</NavLink>
+        </NavItem>
+      </Nav>
 
     </div>
   );
